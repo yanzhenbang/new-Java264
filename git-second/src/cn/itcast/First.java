953d47c137a055f8d36283bb72c2f206153244eb
yanzhenbang <yanzhenbang100515@163.com>
2017-09-22 11:16:59 +0800

    修改First.java

--- git-second/src/cn/itcast/First.java
+++ git-second/src/cn/itcast/First.java
@@ -3,5 +3,8 @@ package cn.itcast;
 public class First {
 	public static void main(String[] args) {
 		System.out.println("hello git!");
+		System.out.println("hello git!");
+		System.out.println("hello git!");
+		System.out.println("hello git!");
 	}
 }
